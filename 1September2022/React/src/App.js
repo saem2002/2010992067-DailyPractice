2a2a0e7343a9e3ecb31577416446afa12c2ab88b
saem <78132205+saem2002@users.noreply.github.com>
2022-09-02 12:47:59 +0530

    1september2022

---
+++ 1September2022/React/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './component/Header';
+
+function App() {
+  return (
+    <div className="container" >
+      <h2>Hello user</h2>
+      <Header />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 1September2022/React/src/component/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>kfv e</div>
+  )
+}
+
+export default Header
