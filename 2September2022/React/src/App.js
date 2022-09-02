bc054b9c18468ba23e6101a417df1eab9cf40c8b
saem <78132205+saem2002@users.noreply.github.com>
2022-09-02 14:26:34 +0530

    2september2022

---
+++ 2September2022/React/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './component/Header';
+
+function App() {
+  return (
+    <div className="container" >
+      <h2>Hello user</h2>
+      <Header  title = "Hi saem"/>
+      <br />
+      <Header  title = "Hi "/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 2September2022/React/src/component/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header(props) {
+  return (
+    <div>I am header {props.title}</div>
+  )
+}
+
+export default Header
